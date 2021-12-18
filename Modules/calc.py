35caf99e30e3b8c6d3822ffb5478365a1c0bd5cc
Abdulrahman-Haji <77126546+ENG-CJ@users.noreply.github.com>
2021-12-18 22:02:28 +0300

    Add files via upload

---
+++ Modules/calc.py
@@ -0,0 +1,20 @@
+
+# File-kaan ayaa iisameysan waxaan rabaa oo kaliya inaan u isicmaalo
+# function kuwwas ii qabanayaa inay numberro isku daraan kala jaraan isku dhuftaan
+
+#  waxaan ugu wici doonaa another file -> calculator ladhaho
+
+# function wuxuu soo celin dooona inuu laba number isku daro makii aan uyeero
+def add(number1,number2):
+    return number1+number2
+
+def subtract(number1,number2):
+    return number1-number2
+
+def multply(number1,number2):
+    return number1*number2
+
+def divide(number1,number2):
+    return number1//number2
+
+# RETURN: dhamaan Function-kaas qiimiyaal ayay soo celinayaa markii aan uyeero
---
+++ Modules/calculator.py
@@ -0,0 +1,29 @@
+
+# file-kii aan soo sameeyay ayaan halkaan import ku dhahayaa si aan u isticmaalo
+# functions-kii aan soo sameeyay
+
+# syntax import file_name
+
+import  calc   # this is the module
+
+number1=int(input('Enter Num1: '))
+number2=int(input('Enter Num2: '))
+
+# halkaan ayaan ugu wacayaa functin-kii aan soo sameeyay
+
+# syntax file_name.function_name
+#👇
+result=calc.add(number1,number2)
+print(result)
+
+#👇
+result2=calc.subtract(number1,number2)
+print(result2)
+
+#👇
+result3=calc.multply(number1,number2)
+print(result3)
+
+#👇
+result4=calc.divide(number1,number2)
+print(result4)
